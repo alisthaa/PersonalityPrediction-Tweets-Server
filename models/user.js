1f8069893a3802104d79fff301dc3e174e967be7
JustSamNotTam <160339041+JustSamNotTam@users.noreply.github.com>
2024-02-23 07:42:07 -0800

    Added fetchUserSchema

--- models/user.js
+++ models/user.js
@@ -1,21 +1,21 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-    first_name: {
-        type: String,
-        required: true
-    },
-    last_name: {
-        type: String,
-        required: true
-    },
-    username: {
-        type: String,
-        required: true,
-        unique: true // Ensures uniqueness of usernames
-    },
+  first_name: {
     type: String,
-    posts: String
+    required: true,
+  },
+  last_name: {
+    type: String,
+    required: true,
+  },
+  username: {
+    type: String,
+    required: true,
+    unique: true, // Ensures uniqueness of usernames
+  },
+  type: String,
+  posts: String,
 });
 
-module.exports = mongoose.model('User', userSchema);
+module.exports = mongoose.model("User", userSchema);
